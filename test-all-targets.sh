889ba5a726f1a16a245fcea2306591cd0dd0bd3a
Silver <silver@silv.re>
2021-11-16 13:07:47 +0000

    Add script to test on (almost) all tier 1 and 2 architectures
    We test Linux, because I run Linux, and Windows, because it's easy to test through Wine.
    A couple tier 2 archs are broken or untestable, so we don't handle those.

---
+++ test-all-targets.sh
@@ -0,0 +1,55 @@
+#!/bin/sh -e
+printf 'native: '
+zig test optz.zig
+
+test_with_runner() {
+	printf '%s: ' "$2"
+	zig test -target "$2" --test-cmd "$1" --test-cmd-bin optz.zig
+}
+test_with_qemu() {
+	case "$1" in
+		sparcv9) set -- sparc64 "$2";;
+		powerpc) set -- ppc "$2";;
+		powerpc64) set -- ppc64 "$2";;
+	esac
+	test_with_runner "qemu-$1-static" "$2"
+}
+test_targets() {
+	grep -v '^#\|^$' | while read -r triple; do
+		echo "$triple" | (
+			IFS=- read -r arch os
+			case "$os" in
+				linux)
+					test_with_qemu "$arch" "$arch-$os-gnu"
+					test_with_qemu "$arch" "$arch-$os-musl"
+					;;
+				windows)
+					test_with_runner wine "$arch-$os"
+					;;
+			esac
+		)
+	done
+}
+
+test_targets <<EOF
+# Tier 1
+x86_64-linux
+x86_64-windows
+
+# Tier 2
+aarch64-linux
+arm-linux
+i386-linux
+mips-linux
+powerpc-linux
+riscv64-linux
+sparcv9-linux
+# std doesn't support (or is broken on) these archs
+#mips64-linux
+#powerpc64-linux
+
+i386-windows
+# Can't use wine for non-native archs
+#aarch64-windows
+#arm-windows
+EOF
