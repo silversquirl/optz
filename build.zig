76c20d43a60dc910af7acb61e42607ddff9a1738
mlugg <mlugg@mlugg.co.uk>
2024-07-19 16:32:29 +0100

    Update to latest Zig
    Tested on Zig 0.14.0-dev.367+a57479afc.

--- build.zig
+++ build.zig
@@ -2,11 +2,6 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("optz", .{
-        .source_file = .{ .path = "optz.zig" },
+        .root_source_file = b.path("optz.zig"),
     });
-
-    const test_step = b.addTest(.{
-        .root_source_file = .{ .path = "optz.zig" },
-    });
-    b.step("test", "Run library tests").dependOn(&test_step.step);
 }
